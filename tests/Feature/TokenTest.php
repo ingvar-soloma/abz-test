a1ac422709cea167be38970a58bf6f49a1cf0d0d
ingvar-soloma <ingvar.soloma@gmail.com>
2025-04-01 01:31:42 +0000

    TokenTest: fix

--- tests/Feature/TokenTest.php
+++ tests/Feature/TokenTest.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\getJson;
 
+uses(RefreshDatabase::class);
+
+
 it('successfully generates a registration token', function () {
-    $response = getJson('/api/token');
+    $response = getJson('/api/v1/token');
 
     $response->assertStatus(200)
         ->assertJsonStructure(['success', 'token'])
@@ -11,7 +15,7 @@ it('successfully generates a registration token', function () {
 
     expect($response->json('token'))->not->toBeEmpty();
 
-    $this->assertDatabaseHas('registration_tokens', [
+    $this->assertDatabaseHas('personal_access_tokens', [
         'token' => hash('sha256', $response->json('token')),
     ]);
 });
