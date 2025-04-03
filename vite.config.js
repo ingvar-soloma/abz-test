b858eb24e80c27e6ac82d8de9c1f2c0326c23530
ingvar-soloma <ingvar.soloma@gmail.com>
2025-04-03 11:30:30 +0000

    setting up Docker for server

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import path from 'path'
+import fs from 'fs';
+
+const host = 'https://abz-test-assignment.vikinglingo.online/';
 
 export default defineConfig({
     plugins: [
@@ -25,6 +28,18 @@ export default defineConfig({
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
             '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+
+    build: {
+        base: process.env.NODE_ENV === 'production' ? '/build/' : '/',
+    },
+    server: {
+        host,
+        hmr: {host},
+        https: {
+            kkey: fs.readFileSync('/etc/ssl/private/my-app.key'),
+            cert: fs.readFileSync('/etc/ssl/certs/my-app.crt'),
         },
     },
 })
